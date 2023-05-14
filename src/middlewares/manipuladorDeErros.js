296c68bd4d5bc5f7739be470631750eacc7e35d6
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2023-05-14 15:27:30 -0300

    adiciona tratamento para erros validação

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -1,14 +1,23 @@
 import mongoose from 'mongoose';
 
 function manipuladorDeErros(err, req, res, next) {
+  // console.error(err);
+
   if (err.message === 'Id do Autor não localizado') {
     res.status(404).json({ message: err.message });
   }
 
   if (err instanceof mongoose.Error.CastError) {
     res.status(400).json({ message: 'Um ou mais dados fornecidos estão incorretos' });
+  } else if (err instanceof mongoose.Error.ValidationError) {
+
+    const mensagensErro = Object.values(err.errors)
+      .map(erro => erro.message)
+      .join('; ');
+
+    res.status(400).json({ message: `Os seguintes erros foram encontrados: ${mensagensErro}` });
   } else {
-    res.status(500).send({ message: 'Erro interno do servidor' });
+    res.status(500).json({ message: 'Erro interno do servidor' });
   }
 }
 
