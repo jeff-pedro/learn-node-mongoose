7410de0294102753bd4a3ee3fcb3ba3dbec58b08
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2023-05-14 13:46:43 -0300

    refatora manipulação de erros do controlador de autores

--- src/controllers/autorController.js
+++ src/controllers/autorController.js
@@ -2,12 +2,12 @@ import autores from '../models/Autor.js';
 
 class AutorController {
 
-  static listarAutores = async (req, res) => {
+  static listarAutores = async (req, res, next) => {
     try {
       const autoresResultado = await autores.find();
       res.status(200).send(autoresResultado);
     } catch (err) {
-      res.status(500).send({ message: err.message });
+      next(err);
     }
   };
 
@@ -28,34 +28,34 @@ class AutorController {
     }
   };
 
-  static cadastrarAutor = async (req, res) => {
+  static cadastrarAutor = async (req, res, next) => {
     const autor = new autores(req.body);
     try {
       const novoAutor = await autor.save();
       res.status(201).send({ message: `Autor ${novoAutor.nome} criado com sucesso.` });
     } catch (err) {
-      res.status(500).send({ message: `${err.message} - Erro ao tentar cadastrar o autor.` });
+      next(err);
     }
   };
 
-  static atualizarAutor = async (req, res) => {
+  static atualizarAutor = async (req, res, next) => {
     const { id } = req.params;
     const novaInfo = req.body;
     try {
       await autores.findByIdAndUpdate(id, { $set: novaInfo });
       res.status(200).send({ message: 'Autor(a) atualizado com sucesso.' });
     } catch (err) {
-      res.status(500).send({ message: `${err.message} - Erro ao atualizar o autor.` });
+      next(err);
     }
   };
 
-  static excluirAutor = async (req, res) => {
+  static excluirAutor = async (req, res, next) => {
     const { id } = req.params;
     try {
       await autores.findByIdAndDelete(id);
       res.status(200).send({ message: `Autor id:${id} excluído.` });
     } catch (err) {
-      res.status(500).send({ message: err.message });
+      next(err);
     }
   };
 }
