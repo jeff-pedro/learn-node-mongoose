b4a028d77327e84ce1ba20a92c31868c89d22e65
Jefferson Pedro <jeffersonpedro89@hotmail.com>
2023-06-10 15:29:03 -0300

    altera parametro de busca para a ordenacao

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -6,7 +6,9 @@ class LivroController {
 
   static listarLivros = async (req, res, next) => {
     try {
-      let { limite = 5, pagina = 1, campoOrdenacao = '_id', ordem = -1 } = req.query;
+      let { limite = 5, pagina = 1, ordenacao = '_id:-1' } = req.query;
+
+      let [campoOrdenacao, ordem] = ordenacao.split(':');
 
       limite = parseInt(limite);
       pagina = parseInt(pagina);
